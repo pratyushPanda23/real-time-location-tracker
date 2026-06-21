a42e9afe686650c082ff5d147a363b77708d58d4
Pratyush Panda <pratyushpanda881@gmail.com>
2026-06-21 09:00:36 +0530

    Initial commit: Real-time location tracker

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", path.join(__dirname, "views"));
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+  console.log("connected");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, (req, res) => {
+  console.log("Server is listening at port 3000");
+});
